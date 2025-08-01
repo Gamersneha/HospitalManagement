e9cf275976b821ace01368d8147929d752c3bf8d
Gamersneha <snehasaha795@gmail.com>
2025-08-02 00:39:56 +0530

    change

--- frontend/register.js
+++ frontend/register.js
@@ -40,7 +40,7 @@ document.getElementById("registerForm").addEventListener("submit", async functio
       status.style.color = "green";
       status.textContent = "Registration successful! Redirecting to login...";
       setTimeout(() => {
-        window.location.href = "login.html";
+        window.location.href = "index.html";
       }, 1500);
     } else {
       status.style.color = "red";
